af30eb431d03660f9be808b98cbaf056dd5ccac3
BeADre <34639100+BeADre@users.noreply.github.com>
2020-10-22 17:21:23 +0800

    fix(loginUsername): add throttle for search (#39)

--- src/utils/common.js
+++ src/utils/common.js
@@ -58,3 +58,13 @@ export function shuffleAList(list) {
   });
   return newSorts;
 }
+
+export function throttle(fn, time) {
+  let isRun = false;
+  return function () {
+    if (isRun) return;
+    isRun = true;
+    fn.apply(this, arguments);
+    setTimeout(() => { isRun = false }, time);
+  }
+}
