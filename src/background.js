e60caf901864865a6eed826baadac87a465f9c04
kunkka <hawtimzhang@gmail.com>
2020-10-24 00:51:29 +0800

    feat: electron supported

---
+++ src/background.js
@@ -0,0 +1,89 @@
+'use strict'
+
+import { app, protocol, BrowserWindow } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } }
+])
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1920,
+    height: 768,
+    webPreferences: {
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
+    }
+  })
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) win.webContents.openDevTools()
+  } else {
+    createProtocol('app')
+    // Load the index.html when not in development
+    win.loadURL('app://./index.html')
+  }
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    try {
+      await installExtension(VUEJS_DEVTOOLS)
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString())
+    }
+  }
+  createWindow()
+})
+
+// Exit cleanly on request from parent process in development mode.
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        app.quit()
+      }
+    })
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit()
+    })
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
       template: "public/index.html",
       filename: "index.html",
       title: "YesPlayMusic",
-      chunks: ["chunk-vendors", "chunk-common", "index"],
+      chunks: ["main", "chunk-vendors", "chunk-common", "index"],
     },
   },
   chainWebpack(config) {
@@ -45,4 +45,73 @@ module.exports = {
       })
       .end();
   },
+  // 添加插件的配置
+  pluginOptions: {
+    // electron-builder的配置文件
+    electronBuilder: {
+      builderOptions: {
+        // files: [
+        //   {
+        //     'filter': ['**/*']
+        //   }
+        // ],
+        // extraFiles: ['./extensions/'],
+        // 应用名称
+        productName: 'Yes Play Music',
+        // 版权
+        copyright: 'Copyright © YesPlayMusic',
+        compression: "maximum",
+        // 是否打包加密
+        asar: true,
+        // // 项目打包生成的文件目录
+        // directories: {
+        //   output: 'build'
+        // },
+        // window的icon头标
+        win: {
+          icon: 'public/favicon.ico'
+        },
+        // 是否静默安装
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true
+        }
+      },
+      // chainWebpackMainProcess: config => {
+        // config.module
+        //   .rule('babel')
+        //   .test(/\*.js$/)
+        //   .use('babel')
+        //   .loader('babel-loader')
+        //   .options({
+        //     presets: [['@babel/preset-env', '@vue/cli-plugin-babel/preset', { modules: false }]],
+        //     plugins: ['@babel/plugin-proposal-class-properties']
+        //   })
+      // },
+      // 渲染线程的配置文件
+      // chainWebpackRendererProcess: config => {
+      //   // 渲染线程的一些其他配置
+      //   // Chain webpack config for electron renderer process only
+      //   // The following example will set IS_ELECTRON to true in your app
+      //   config.plugin('define').tap((args) => {
+      //     args[0]['IS_ELECTRON'] = true
+      //     return args
+      //   })
+      // },
+      // 主入口文件
+      // mainProcessFile: 'src/main.js',
+      // mainProcessWatch: [],
+      // mainProcessArgs: []
+    }
+  },
+  // 打包时不生成.map文件，减少体积，加快速度如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false,
+  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+  assetsDir: 'src/renderer/static',
+  // 跨域配置
+  devServer: {
+    disableHostCheck: true
+  },
+  // css 样式
+  css: {}
 };
