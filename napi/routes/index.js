445aaf926f4732a779beed9430e70320c82e1c27
kunkka <hawtimzhang@gmail.com>
2020-10-27 23:33:24 +0800

    fix: path

--- napi/routes/index.js
+++ napi/routes/index.js
@@ -1,5 +1,5 @@
-const { cookieToJson } = require('./util/index');
-const request = require('./util/request');
+const { cookieToJson } = require('../util/index');
+const request = require('../util/request');
 module.exports = {
   '/yunbei/today': (req, res) => {
     if (typeof req.query.cookie === 'string') {
