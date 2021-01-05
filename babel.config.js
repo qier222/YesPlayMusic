0482e6a1edfc8b2c0dfb733cb424dc6a99633a7f
qier222 <qier222@outlook.com>
2021-01-05 16:06:22 +0800

    chore: update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    "@babel/plugin-proposal-nullish-coalescing-operator",
+    "@babel/plugin-proposal-optional-chaining",
+  ],
 };
