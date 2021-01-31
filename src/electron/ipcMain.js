384451c25723eda7e83a9a6106c8382cace0412e
TestGifts <admin@test.gifts>
2021-01-31 14:06:58 +0800

    Update ipcMain.js
    Add close pop-up confirmation

--- src/electron/ipcMain.js
+++ src/electron/ipcMain.js
@@ -25,10 +25,27 @@ export function initIpcMain(win, store) {
       });
   });
 
-  ipcMain.on("close", () => {
-    win.hide();
-    // win.close();
-    // app.quit();
+  ipcMain..on('close', (e) => {
+    e.preventDefault()//阻止默认行为，一定要有
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Information',
+      cancelId:2,
+      defaultId: 0,
+      message: '确定要关闭吗？',
+      buttons: ['最小化','直接退出']
+    }).then(result => {
+      if (result.response == 0) {
+        e.preventDefault();		//阻止默认行为
+        win.minimize();	//调用 最小化实例方法
+      } else if(result.response == 1) {
+        win = null;
+        //app.quit();	
+        app.exit();		//exit()直接关闭客户端，不会执行quit();
+      }
+    }).catch(err => {
+      console.log(err)
+    })
   });
 
   ipcMain.on("minimize", () => {
