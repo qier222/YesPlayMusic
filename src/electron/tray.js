ea52c32400485d8645cc8cfe623b785b0432eec2
TestGifts <admin@test.gifts>
2021-02-08 20:24:26 +0800

    Expand tray menu
    增加了托盘菜单图标及功能

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -22,26 +22,54 @@ export function createTray(win) {
     const contextMenu = Menu.buildFromTemplate([
       {
           label: "播放/暂停",
+          icon:  "src/icon/play.png",
           click: () => {
             win.webContents.send("play");
           },
         },
         {
+          label: "上一首",
+          icon:  "src/icon/left.png",
+          accelerator: "CmdOrCtrl+Left",
+          click: () => {
+            win.webContents.send("previous");
+          },
+        },
+        {
           label: "下一首",
+          icon:  "src/icon/right.png",
           accelerator: "CmdOrCtrl+Right",
           click: () => {
             win.webContents.send("next");
           },
         },
         {
+          label: "循环播放",
+          icon:  "src/icon/repeat.png",
+          accelerator: "Alt+R",
+          click: () => {
+            win.webContents.send("repeat");
+          },
+        },
+        {
+          label: "加入喜欢",
+          icon:  "src/icon/like.png",
+          accelerator: "CmdOrCtrl+L",
+          click: () => {
+            win.webContents.send("like");
+          },
+        },
+        {
           label: "退出",
+          icon:  "src/icon/exit.png",
+          accelerator: "CmdOrCtrl+W",
           click: () => {
             app.exit();
           },
         },
     ]);
+    
     tray.popUpContextMenu(contextMenu);
   });
 
   return tray;
-}
