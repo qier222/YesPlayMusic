c6ddf9ac3f79dd997b1d5145fcc1ed83201d5b60
qier222 <qier222@outlook.com>
2021-02-15 23:40:09 +0800

    chore: use devServer.proxy

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,15 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
     port: process.env.DEV_SERVER_PORT || 8080,
+    proxy: {
+      "^/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/",
+        },
+      },
+    },
   },
   pwa: {
     name: "YesPlayMusic",
