4e023502cafc29db02bbab065f8a88e8798521fb
qier222 <qier222@outlook.com>
2021-04-19 11:33:29 +0800

    fix: remove sentry

--- src/main.js
+++ src/main.js
@@ -9,10 +9,6 @@ import "@/utils/filters";
 import "./registerServiceWorker";
 import { dailyTask } from "@/utils/common";
 
-import * as Sentry from "@sentry/browser";
-import { Vue as VueIntegration } from "@sentry/integrations";
-import { Integrations } from "@sentry/tracing";
-
 window.resetApp = () => {
   localStorage.clear();
   indexedDB.deleteDatabase("yesplaymusic");
@@ -36,24 +32,6 @@ Vue.use(VueAnalytics, {
 });
 
 Vue.config.productionTip = false;
-
-if (process.env.VUE_APP_ENABLE_SENTRY === "true") {
-  Sentry.init({
-    dsn:
-      "https://30aaa25152974f48971912a394ab6bc3@o436528.ingest.sentry.io/5477409",
-    integrations: [
-      new VueIntegration({
-        Vue,
-        tracing: true,
-      }),
-      new Integrations.BrowserTracing(),
-    ],
-
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-  });
-}
 
 dailyTask();
 
