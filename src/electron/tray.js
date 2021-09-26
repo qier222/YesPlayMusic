093f588df62e34d67f31147e13694253ceabac78
memorydream <memorydream.chen@outlook.com>
2021-09-26 11:17:11 +0800

    try tray context on linux

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -1,6 +1,6 @@
 /* global __static */
 import path from 'path';
-import { app, nativeImage, Tray, Menu } from 'electron';
+import { app, nativeImage, Tray, Menu, MenuItem } from 'electron';
 
 export function createTray(win) {
   let icon = nativeImage
@@ -9,79 +9,102 @@ export function createTray(win) {
       height: 20,
       width: 20,
     });
-  let tray = new Tray(icon);
 
+  let contextMenu = Menu.buildFromTemplate([
+    {
+      label: '播放/暂停',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/play.png')
+      ),
+      click: () => {
+        win.webContents.send('play');
+      },
+    },
+    {
+      label: '上一首',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/left.png')
+      ),
+      accelerator: 'CmdOrCtrl+Left',
+      click: () => {
+        win.webContents.send('previous');
+      },
+    },
+    {
+      label: '下一首',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/right.png')
+      ),
+      accelerator: 'CmdOrCtrl+Right',
+      click: () => {
+        win.webContents.send('next');
+      },
+    },
+    {
+      label: '循环播放',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/repeat.png')
+      ),
+      accelerator: 'Alt+R',
+      click: () => {
+        win.webContents.send('repeat');
+      },
+    },
+    {
+      label: '加入喜欢',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/like.png')
+      ),
+      accelerator: 'CmdOrCtrl+L',
+      click: () => {
+        win.webContents.send('like');
+      },
+    },
+    {
+      label: '退出',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/exit.png')
+      ),
+      accelerator: 'CmdOrCtrl+W',
+      click: () => {
+        app.exit();
+      },
+    },
+  ]);
+
+  let tray = new Tray(icon);
   tray.setToolTip('YesPlayMusic');
 
-  tray.on('click', () => {
-    win.show();
-  });
+  if (process.platform === 'linux') {
+    //linux下托盘的实现方式比较迷惑
+    //right-click无法在linux下使用
+    //click在默认行为下会弹出一个contextMenu，里面的唯一选项才会调用click事件
+    //setContextMenu应该是目前唯一能在linux下使用托盘菜单api
+    //但是无法区分鼠标左右键
 
-  tray.on('right-click', () => {
-    const contextMenu = Menu.buildFromTemplate([
-      {
-        label: '播放/暂停',
-        icon: nativeImage.createFromPath(
-          path.join(__static, 'img/icons/play.png')
-        ),
-        click: () => {
-          win.webContents.send('play');
-        },
+    //setContextMenu破坏了预期的click行为
+    //在linux下，最小化到托盘的行为貌似与最小化一致
+    //所以可能需要一个从托盘图标还原程序窗口的选项
+    let item = new MenuItem({
+      label: '显示主面板',
+      icon: nativeImage.createFromPath(
+        path.join(__static, 'img/icons/menu.png')
+      ),
+      click: () => {
+        win.show();
       },
-      {
-        label: '上一首',
-        icon: nativeImage.createFromPath(
-          path.join(__static, 'img/icons/left.png')
-        ),
-        accelerator: 'CmdOrCtrl+Left',
-        click: () => {
-          win.webContents.send('previous');
-        },
-      },
-      {
-        label: '下一首',
-        icon: nativeImage.createFromPath(
-          path.join(__static, 'img/icons/right.png')
-        ),
-        accelerator: 'CmdOrCtrl+Right',
-        click: () => {
-          win.webContents.send('next');
-        },
-      },
-      {
-        label: '循环播放',
-        icon: nativeImage.createFromPath(
-          path.join(__static, 'img/icons/repeat.png')
-        ),
-        accelerator: 'Alt+R',
-        click: () => {
-          win.webContents.send('repeat');
-        },
-      },
-      {
-        label: '加入喜欢',
-        icon: nativeImage.createFromPath(
-          path.join(__static, 'img/icons/like.png')
-        ),
-        accelerator: 'CmdOrCtrl+L',
-        click: () => {
-          win.webContents.send('like');
-        },
-      },
-      {
-        label: '退出',
-        icon: nativeImage.createFromPath(
-          path.join(__static, 'img/icons/exit.png')
-        ),
-        accelerator: 'CmdOrCtrl+W',
-        click: () => {
-          app.exit();
-        },
-      },
-    ]);
+    });
+    contextMenu.append(item);
+    tray.setContextMenu(contextMenu);
+  } else {
+    tray.on('click', () => {
+      win.show();
+    });
 
-    tray.popUpContextMenu(contextMenu);
-  });
+    tray.on('right-click', () => {
+      tray.popUpContextMenu(contextMenu);
+    });
+  }
 
   return tray;
 }
