6871e3e365fbb9b7082983e883765b2210068425
memorydream <34763046+memorydream@users.noreply.github.com>
2021-09-27 13:31:46 +0800

    调整linux下托盘菜单的选项位置

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -10,7 +10,54 @@ export function createTray(win) {
       width: 20,
     });
 
-  let contextMenu = Menu.buildFromTemplate([
+  let contextMenu = new Menu();
+  let tray = new Tray(icon);
+  tray.setToolTip('YesPlayMusic');
+
+  if (process.platform === 'linux') {
+    //linux下托盘的实现方式比较迷惑
+    //right-click无法在linux下使用
+    //click在默认行为下会弹出一个contextMenu，里面的唯一选项才会调用click事件
+    //setContextMenu应该是目前唯一能在linux下使用托盘菜单api
+    //但是无法区分鼠标左右键
+
+    //setContextMenu破坏了预期的click行为
+    //在linux下，最小化到托盘的行为貌似与最小化一致
+    //所以此处添加一个显示主面板的按钮
+    contextMenu.append([
+      {
+        label: '显示主面板',
+        icon: nativeImage.createFromPath(
+          path.join(__static, 'img/icons/menu.png')
+        ),
+        click: () => {
+          win.show();
+        },
+      },
+      {
+        type: 'separator',
+      },
+    ]);
+    appendCommonTrayContextItems(win, contextMenu);
+    tray.setContextMenu(contextMenu);
+  } else {
+    //windows and macos
+    appendCommonTrayContextItems(win, contextMenu);
+
+    tray.on('click', () => {
+      win.show();
+    });
+
+    tray.on('right-click', () => {
+      tray.popUpContextMenu(contextMenu);
+    });
+  }
+
+  return tray;
+}
+
+function appendCommonTrayContextItems(win, contextMenu) {
+  contextMenu.append([
     {
       label: '播放/暂停',
       icon: nativeImage.createFromPath(
@@ -71,40 +118,4 @@ export function createTray(win) {
       },
     },
   ]);
-
-  let tray = new Tray(icon);
-  tray.setToolTip('YesPlayMusic');
-
-  if (process.platform === 'linux') {
-    //linux下托盘的实现方式比较迷惑
-    //right-click无法在linux下使用
-    //click在默认行为下会弹出一个contextMenu，里面的唯一选项才会调用click事件
-    //setContextMenu应该是目前唯一能在linux下使用托盘菜单api
-    //但是无法区分鼠标左右键
-
-    //setContextMenu破坏了预期的click行为
-    //在linux下，最小化到托盘的行为貌似与最小化一致
-    //所以可能需要一个从托盘图标还原程序窗口的选项
-    let item = new MenuItem({
-      label: '显示主面板',
-      icon: nativeImage.createFromPath(
-        path.join(__static, 'img/icons/menu.png')
-      ),
-      click: () => {
-        win.show();
-      },
-    });
-    contextMenu.append(item);
-    tray.setContextMenu(contextMenu);
-  } else {
-    tray.on('click', () => {
-      win.show();
-    });
-
-    tray.on('right-click', () => {
-      tray.popUpContextMenu(contextMenu);
-    });
-  }
-
-  return tray;
 }
