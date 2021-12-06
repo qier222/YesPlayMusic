0d3df4a1e4f44adafe8a52b81623bf3e34c22262
是虹川飴 <r0akg9yxh@relay.firefox.com>
2021-12-06 18:27:24 +0800

    fix: 修复 issues #1019 的跟进问题 (#1068)
    * issues #1019 的迫真修复
    
    * 修复 issues #1019
    
    * 修复 issues #1019
    
    * 改回 pickedLyric() 逻辑
    
    * 更进一步的避免取词卡死问题
    
    * 更新网易云 api 到 4.2.0
    
    * Update README.md
    
    * Revert "Update README.md"
    
    This reverts commit b862ef7d4dabd40c8fe57e4837fc6220806a1456.

--- netease_api/module/record_recent_song.js
+++ netease_api/module/record_recent_song.js
@@ -1,14 +1,10 @@
-// 获取动态评论
-
 module.exports = (query, request) => {
   const data = {
-    limit: query.limit || 20,
-    offset: query.offset || 0,
-    beforeTime: query.before || 0,
+    limit: query.limit || 100,
   }
   return request(
     'POST',
-    `https://music.163.com/weapi/v1/resource/comments/${query.threadId}`,
+    `https://music.163.com/api/play-record/song/list`,
     data,
     {
       crypto: 'weapi',
