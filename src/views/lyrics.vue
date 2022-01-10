58ac5b66a13e652a743c18775a713b781e4b3a17
memorydream <34763046+memorydream@users.noreply.github.com>
2022-01-10 09:09:10 +0800

    better author filter

--- src/views/lyrics.vue
+++ src/views/lyrics.vue
@@ -334,16 +334,14 @@ export default {
             lyric.length <= 10 &&
             lyric.map(l => l.content).includes('纯音乐，请欣赏');
           if (includeAM) {
-            try {
-              let r = new RegExp(
-                `^作(词|曲)\\s*(:|：)\\s*${this.currentTrack?.ar[0]?.name}$`
+            let reg = /^作(词|曲)\s*(:|：)\s*/;
+            let author = this.currentTrack?.ar[0]?.name;
+            lyric = lyric.filter(l => {
+              let regExpArr = l.content.match(reg);
+              return (
+                !regExpArr || l.content.replace(regExpArr[0], '') !== author
               );
-              lyric = lyric.filter(l => !r.exec(l.content));
-            } catch {
-              console.log(
-                `can not match author ${this.currentTrack?.ar[0]?.name}, skip`
-              );
-            }
+            });
           }
           if (lyric.length === 1 && includeAM) {
             this.lyric = [];
