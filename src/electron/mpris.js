adb0939d5ecec11e9f92d6634c60576171d9fdd1
CNawalol <awalol@aliyun.com>
2022-01-27 19:56:08 +0800

    fix: 修正mpris的name与identity

--- src/electron/mpris.js
+++ src/electron/mpris.js
@@ -5,8 +5,8 @@ export function createMpris(window) {
   const renderer = window.webContents;
 
   const player = Player({
-    name: 'hellow',
-    identity: 'Hello',
+    name: 'yesplaymusic',
+    identity: 'YesPlayMusic media palyer',
   });
 
   player.on('next', () => renderer.send('next'));
