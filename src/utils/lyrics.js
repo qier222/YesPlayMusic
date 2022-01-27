ac203da6816d332f9a4d0d55d1ba7250b4f6b1e1
pan93412 <pan93412@gmail.com>
2022-01-27 11:06:36 +0800

    refactor(utils/lyrics): support multi-timestamp lyrics
    Fully rewrited and improved the performance.

--- src/utils/lyrics.js
+++ src/utils/lyrics.js
@@ -9,38 +9,86 @@ export function lyricParser(lrc) {
   };
 }
 
-const extractTimeRegex = /^(?<rawTime>\[(?<min>\d+):(?<sec>\d+)(?:\.|:)(?<ms>\d+)\])\s*(?<content>.+)$/;
+const extractLrcRegex = /^(?<lyricTimestamps>(?:\[.+?]\s*)+)(?<content>.+)$/gm;
+const extractTimestampRegex = /\[(?<min>\d+):(?<sec>\d+)(?:\.|:)(?<ms>\d+)\]/g;
 
+/**
+ * @typedef {{time: number, rawTime: string}} ParsedTimestamp
+ */
+/**
+ * @typedef {{content: string} & ParsedTimestamp} ParsedLyric
+ */
+
+/**
+ * Parse the lyric string.
+ *
+ * @param {string} lrc The `lrc` input.
+ * @returns {ParsedLyric[]} The parsed lyric.
+ * @example parseLyric("[00:00.00] Hello, World!\n[00:00.10] Test\n");
+ */
 function parseLyric(lrc) {
-  const lyrics = lrc.trim().split('\n');
+  /**
+   * We use a binary tree to store a list of parsed lyric and its timestamp.
+   *
+   * @type {ParsedLyric[]}
+   * @see binarySearch
+   */
+  const parsedLyrics = [];
 
-  const parsedLyrics = lyrics
-    .map((/** @type {string} */ line) => {
-      try {
-        const extractedLyric = extractTimeRegex.exec(line);
+  /**
+   * Find the appropriate index to push our parsed lyric.
+   * @param {ParsedLyric} lyric
+   */
+  const binarySearch = lyric => {
+    let time = lyric.time;
 
-        // If this line is not a lyric.
-        if (!extractedLyric) throw 'This line is not a valid lyric.';
+    let low = 0;
+    let high = parsedLyrics.length - 1;
 
-        // Otherwise, we extract the lyric part.
-        const { rawTime, min, sec, ms, content } = extractedLyric.groups;
-        const time = Number(min) * 60 + Number(sec) + 0.01 * Number(ms);
-
-        return {
-          time,
-          rawTime,
-          content: trimContent(content),
-        };
-      } catch (e) {
-        console.debug(`lyrics.js: Failed to extract "${line}". ${e}`);
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+      const midTime = parsedLyrics[mid].time;
+      if (midTime === time) {
+        return mid;
+      } else if (midTime < time) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
       }
-    })
-    .filter(response => !!response) // remove "undefined" entries
-    .sort((a, b) => a.time - b.time);
+    }
+
+    return low;
+  };
+
+  for (const line of lrc.trim().matchAll(extractLrcRegex)) {
+    /** @type {ParsedTimestamp[]} */
+    const timestamps = [];
+    const { lyricTimestamps, content } = line.groups;
+
+    for (const timestamp of lyricTimestamps.matchAll(extractTimestampRegex)) {
+      const { min, sec, ms } = timestamp.groups;
+      const rawTime = timestamp[0];
+      const time = Number(min) * 60 + Number(sec) + 0.001 * Number(ms);
+
+      timestamps.push({ time, rawTime });
+    }
+
+    const parsedLyric = timestamps.map(timestamp => ({
+      ...timestamp,
+      content: trimContent(content),
+    }));
+    parsedLyric.forEach(lyric =>
+      parsedLyrics.splice(binarySearch(lyric), 0, lyric)
+    );
+  }
 
   return parsedLyrics;
 }
 
+/**
+ * @param {string} content
+ * @returns {string}
+ */
 function trimContent(content) {
   let t = content.trim();
   return t.length < 1 ? content : t;
