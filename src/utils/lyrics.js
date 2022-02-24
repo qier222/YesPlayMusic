fe14bf4991d261f1780af4a1cd9111abaddf3796
pan93412 <pan93412@gmail.com>
2022-02-24 13:20:14 +0800

    style: prettier

--- src/utils/lyrics.js
+++ src/utils/lyrics.js
@@ -8,8 +8,10 @@ export function lyricParser(lrc) {
 }
 
 // regexr.com/6e52n
-const extractLrcRegex = /^(?<lyricTimestamps>(?:\[.+?\])+)(?!\[)(?<content>.+)$/gm;
-const extractTimestampRegex = /\[(?<min>\d+):(?<sec>\d+)(?:\.|:)*(?<ms>\d+)*\]/g;
+const extractLrcRegex =
+  /^(?<lyricTimestamps>(?:\[.+?\])+)(?!\[)(?<content>.+)$/gm;
+const extractTimestampRegex =
+  /\[(?<min>\d+):(?<sec>\d+)(?:\.|:)*(?<ms>\d+)*\]/g;
 
 /**
  * @typedef {{time: number, rawTime: string, content: string}} ParsedLyric
