748db54f52c8179a62254afcdb22cd0de131b4a2
memorydream <34763046+memorydream@users.noreply.github.com>
2022-03-16 15:45:36 +0800

    fix(utils/platform): isCreateTray 在瀏覽器環境應永遠為應永遠為 false

--- src/utils/platform.js
+++ src/utils/platform.js
@@ -3,5 +3,6 @@ export const isMac = process.platform === 'darwin';
 export const isLinux = process.platform === 'linux';
 export const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const isCreateTray = isWindows || isLinux || isDevelopment;
+export const isCreateTray =
+  process.env.IS_ELECTRON && (isWindows || isLinux || isDevelopment);
 export const isCreateMpris = isLinux;
