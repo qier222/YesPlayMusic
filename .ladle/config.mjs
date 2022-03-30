57f5d5bdc6d63d1a28aa636bb61a10b6618400d6
pan93412 <pan93412@gmail.com>
2022-03-30 09:28:55 +0800

    fix(config/ladle): set '@' to renderer source

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,3 +1,8 @@
 export default {
     stories: "src/renderer/**/*.stories.{jsx,tsx}",
+    resolve: {
+        alias: {
+            '@': new URL('../src/renderer', import.meta.url).pathname,
+        },
+    },
 };
