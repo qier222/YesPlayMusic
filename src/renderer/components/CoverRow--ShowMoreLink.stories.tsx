54388e67f10a44e301ecfd685c1c19f5f831abf9
pan93412 <pan93412@gmail.com>
2022-03-31 16:08:23 +0800

    refactor(comps): child components in CoverRow

---
+++ src/renderer/components/CoverRow--ShowMoreLink.stories.tsx
@@ -0,0 +1,3 @@
+import { ShowMoreLink } from "./CoverRow";
+
+export const SeeMoreLinkStory = () => <ShowMoreLink href="https://www.example.com" />;
--- src/renderer/components/CoverRow.tsx
+++ src/renderer/components/CoverRow.tsx
@@ -1,9 +1,12 @@
+import React, { useMemo } from 'react'
 import Cover from '@/components/Cover'
 import Skeleton from '@/components/Skeleton'
 import SvgIcon from '@/components/SvgIcon'
 import { prefetchAlbum } from '@/hooks/useAlbum'
 import { prefetchPlaylist } from '@/hooks/usePlaylist'
 import { formatDate, resizeImage, scrollToTop } from '@/utils/common'
+import classNames from 'classnames'
+import { useNavigate } from 'react-router-dom'
 
 export enum Subtitle {
   COPYWRITER = 'copywriter',
@@ -12,27 +15,61 @@ export enum Subtitle {
   ARTIST = 'artist',
 }
 
-const Title = ({
-  title,
-  seeMoreLink,
-}: {
-  title: string
-  seeMoreLink: string
-}) => {
-  return (
-    <div className='flex items-baseline justify-between'>
-      <div className='my-4 text-[28px] font-bold text-black dark:text-white'>
-        {title}
-      </div>
-      {seeMoreLink && (
-        <div className='text-13px font-semibold text-gray-600 hover:underline'>
-          See More
-        </div>
-      )}
-    </div>
-  )
+export interface MoreLinkProps extends React.ComponentPropsWithoutRef<'a'> {
+  href: string;
 }
 
+export const ShowMoreLink = ({ className, ...props }: MoreLinkProps) => (
+  <a className={classNames('font-semibold text-gray-600 text-13px hover:underline cursor-pointer', className)} {...props}>
+    See More
+  </a>
+)
+
+export interface TitleProps {
+  title: string
+  showMoreLink: string
+}
+
+export const Title = ({ title, showMoreLink }: TitleProps) => (
+  <section className='flex items-baseline justify-between'>
+    <div className='my-4 text-[28px] font-bold text-black dark:text-white'>
+      {title}
+    </div>
+    {showMoreLink && <ShowMoreLink href={showMoreLink} />}
+  </section>
+)
+
+/**
+ * 將從網易雲收到的標籤，轉換為 YesPlayMusic
+ * 統一的類型標籤。
+ * 
+ * @param type 從網易雲接收到的標籤。
+ * @returns YesPlayMusic 的類型標籤。
+ */
+const remapType = (
+  type: string
+): 'playlist' | 'Album' | 'EP' | 'Collection' | 'unknown' => {
+  switch (type) {
+    case 'playlist':
+      return 'playlist'
+    case 'album':
+    case '專輯':
+    case '专辑':
+      return 'Album'
+    case 'EP':
+    case 'EP/Single':
+      return 'EP'
+    case '精選集':
+    case '精选集':
+      return 'Collection'
+    default:
+      return 'unknown'
+  }
+}
+
+/**
+ * 取得副標題內容。
+ */
 const getSubtitleText = (
   item: Album | Playlist | Artist,
   subtitle: Subtitle
@@ -40,21 +77,12 @@ const getSubtitleText = (
   const nickname = 'creator' in item ? item.creator.nickname : 'someone'
   const artist = 'artist' in item ? item.artist.name : 'unknown'
   const copywriter = 'copywriter' in item ? item.copywriter : 'unknown'
-  const releaseYear =
-    ('publishTime' in item &&
-      formatDate(item.publishTime ?? 0, 'en', 'YYYY')) ||
-    'unknown'
-
-  const type = {
-    playlist: 'playlist',
-    album: 'Album',
-    专辑: 'Album',
-    Single: 'Single',
-    'EP/Single': 'EP',
-    EP: 'EP',
-    unknown: 'unknown',
-    精选集: 'Collection',
-  }[('type' in item && typeof item.type !== 'number' && item.type) || 'unknown']
+  const releaseYear = 'publishTime' in item
+    ? formatDate(item.publishTime ?? 0, 'en', 'YYYY')
+    : 'unknown';
+  const type = ('type' in item && typeof item.type === 'string')
+    ? remapType(item.type)
+    : 'unknown';
 
   const table = {
     [Subtitle.CREATOR]: `by ${nickname}`,
@@ -66,12 +94,39 @@ const getSubtitleText = (
   return table[subtitle]
 }
 
+/**
+ * 取得影像的 URL。
+ * 
+ * @param item 專輯、播放列表或作家的資料。
+ * @returns 影像 URL。如果沒有，則回傳 `null`。
+ */
 const getImageUrl = (item: Album | Playlist | Artist) => {
-  let cover: string | undefined = ''
-  if ('coverImgUrl' in item) cover = item.coverImgUrl
-  if ('picUrl' in item) cover = item.picUrl
-  if ('img1v1Url' in item) cover = item.img1v1Url
-  return resizeImage(cover || '', 'md')
+  let cover: string | undefined;
+
+  if ('coverImgUrl' in item && item.coverImgUrl) {
+    cover = item.coverImgUrl
+  } else if ('picUrl' in item && item.picUrl) {
+    cover = item.picUrl
+  } else if ('img1v1Url' in item && item.img1v1Url) {
+    cover = item.img1v1Url
+  }
+
+  return cover
+    ? resizeImage(cover, 'md')
+    : null;
+}
+
+export interface CoverRowProps {
+  title?: string
+  albums?: Album[]
+  artists?: Artist[]
+  playlists?: Playlist[]
+  subtitle?: Subtitle
+  seeMoreLink?: string
+  isSkeleton?: boolean
+  className?: string
+  rows?: number
+  navigateCallback?: () => void
 }
 
 const CoverRow = ({
@@ -85,18 +140,7 @@ const CoverRow = ({
   className,
   rows = 2,
   navigateCallback, // Callback function when click on the cover/title
-}: {
-  title?: string
-  albums?: Album[]
-  artists?: Artist[]
-  playlists?: Playlist[]
-  subtitle?: Subtitle
-  seeMoreLink?: string
-  isSkeleton?: boolean
-  className?: string
-  rows?: number
-  navigateCallback?: () => void
-}) => {
+}: CoverRowProps) => {
   const renderItems = useMemo(() => {
     if (isSkeleton) {
       return new Array(rows * 5).fill({}) as Array<Album | Playlist | Artist>
@@ -121,7 +165,7 @@ const CoverRow = ({
 
   return (
     <div>
-      {title && <Title title={title} seeMoreLink={seeMoreLink ?? ''} />}
+      {title && <Title title={title} showMoreLink={seeMoreLink ?? ''} />}
 
       <div
         className={classNames(
@@ -140,7 +184,7 @@ const CoverRow = ({
             <div>
               {/*  Cover  */}
               {isSkeleton ? (
-                <Skeleton className='box-content aspect-square w-full rounded-xl border border-black border-opacity-0' />
+                <Skeleton className='box-content w-full border border-black border-opacity-0 aspect-square rounded-xl' />
               ) : (
                 <Cover
                   onClick={() => goTo(item.id)}
@@ -154,21 +198,21 @@ const CoverRow = ({
                 <div className='font-semibold'>
                   {/*  Name */}
                   {isSkeleton ? (
-                    <div className='flex w-full -translate-y-px flex-col'>
+                    <div className='flex flex-col w-full -translate-y-px'>
                       <Skeleton className='w-full leading-tight'>
                         PLACEHOLDER
                       </Skeleton>
-                      <Skeleton className='w-1/3 translate-y-px leading-tight'>
+                      <Skeleton className='w-1/3 leading-tight translate-y-px'>
                         PLACEHOLDER
                       </Skeleton>
                     </div>
                   ) : (
-                    <span className='line-clamp-2 leading-tight '>
+                    <span className='leading-tight line-clamp-2 '>
                       {/* Playlist private icon */}
                       {(item as Playlist).privacy && (
                         <SvgIcon
                           name='lock'
-                          className='mr-1 mb-1 inline-block h-3 w-3 text-gray-300'
+                          className='inline-block w-3 h-3 mb-1 mr-1 text-gray-300'
                         />
                       )}
 
