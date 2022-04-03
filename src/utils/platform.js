871217713a8312fc6e40659c100fc45f93ff6954
pan93412 <pan93412@gmail.com>
2022-04-04 00:46:08 +0800

    revert: isCreateTray 在瀏覽器環境應永遠為應永遠為 false
    部分環境沒有 process.env.IS_ELECTRON，以致發生錯誤。
    
    This reverts commit 748db54f52c8179a62254afcdb22cd0de131b4a2.

--- src/utils/platform.js
+++ src/utils/platform.js
@@ -3,6 +3,5 @@ export const isMac = process.platform === 'darwin';
 export const isLinux = process.platform === 'linux';
 export const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const isCreateTray =
-  process.env.IS_ELECTRON && (isWindows || isLinux || isDevelopment);
+export const isCreateTray = isWindows || isLinux || isDevelopment;
 export const isCreateMpris = isLinux;
