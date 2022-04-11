534a43e170bc8055f0086b27c5731fc660c3c1ee
Restyled.io <commits@restyled.io>
2022-04-11 13:31:00 +0000

    Restyled by clang-format

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { getCookie, doLogout } from '@/utils/auth';
 import router from '@/router';
+import {doLogout, getCookie} from '@/utils/auth';
+import axios from 'axios';
 
 let baseURL = '';
 // Web 和 Electron 跑在不同端口避免同时启动时冲突
@@ -16,12 +16,13 @@ if (process.env.IS_ELECTRON) {
 
 const service = axios.create({
   baseURL,
-  withCredentials: true,
-  timeout: 15000,
+  withCredentials : true,
+  timeout : 15000,
 });
 
-service.interceptors.request.use(function (config) {
-  if (!config.params) config.params = {};
+service.interceptors.request.use(function(config) {
+  if (!config.params)
+    config.params = {};
   if (baseURL.length) {
     if (baseURL[0] !== '/' && !process.env.IS_ELECTRON) {
       config.params.cookie = `MUSIC_U=${getCookie('MUSIC_U')};`;
@@ -39,7 +40,7 @@ service.interceptors.request.use(function (config) {
   }
 
   const proxy = JSON.parse(localStorage.getItem('settings')).proxyConfig;
-  if (['HTTP', 'HTTPS'].includes(proxy.protocol)) {
+  if ([ 'HTTP', 'HTTPS' ].includes(proxy.protocol)) {
     config.params.proxy = `${proxy.protocol}://${proxy.server}:${proxy.port}`;
   }
 
@@ -47,34 +48,29 @@ service.interceptors.request.use(function (config) {
 });
 
 service.interceptors.response.use(
-  response => {
-    const res = response.data;
-    return res;
-  },
-  async error => {
-    /** @type {import('axios').AxiosResponse | null} */
-    const response = error.response;
-    const data = response.data;
+    response => {
+      const res = response.data;
+      return res;
+    },
+    async error => {
+      /** @type {import('axios').AxiosResponse | null} */
+      const response = error.response;
+      const data = response.data;
 
-    if (
-      response &&
-      typeof data === 'object' &&
-      data.code === 301 &&
-      data.msg === '需要登录'
-    ) {
-      console.warn('Token has expired. Logout now!');
+      if (response && typeof data === 'object' && data.code === 301 &&
+          data.msg === '需要登录') {
+        console.warn('Token has expired. Logout now!');
 
-      // 登出帳戶
-      doLogout();
+        // 登出帳戶
+        doLogout();
 
-      // 導向登入頁面
-      if (process.env.IS_ELECTRON === true) {
-        router.push({ name: 'loginAccount' });
-      } else {
-        router.push({ name: 'login' });
+        // 導向登入頁面
+        if (process.env.IS_ELECTRON === true) {
+          router.push({name : 'loginAccount'});
+        } else {
+          router.push({name : 'login'});
+        }
       }
-    }
-  }
-);
+    });
 
 export default service;
