67a22dfccd818bcd4c43d38e40af2cb6a6de1d13
Restyled.io <commits@restyled.io>
2022-04-15 18:17:21 +0000

    Restyled by prettier

--- src/electron/ipcMain.js
+++ src/electron/ipcMain.js
@@ -1,70 +1,76 @@
-import {registerGlobalShortcut} from '@/electron/globalShortcut';
-import {isCreateTray, isMac} from '@/utils/platform';
+import { registerGlobalShortcut } from '@/electron/globalShortcut';
+import { isCreateTray, isMac } from '@/utils/platform';
 import shortcuts from '@/utils/shortcuts';
 // import match from '@unblockneteasemusic/server';
 import UNM from '@unblockneteasemusic/rust-napi';
-import {app, dialog, globalShortcut, ipcMain} from 'electron';
+import { app, dialog, globalShortcut, ipcMain } from 'electron';
 import cloneDeep from 'lodash/cloneDeep';
 
-import {createMenu} from './menu';
+import { createMenu } from './menu';
 
 const clc = require('cli-color');
-const log =
-    text => { console.log(`${clc.blueBright('[ipcMain.js]')} ${text}`); };
+const log = text => {
+  console.log(`${clc.blueBright('[ipcMain.js]')} ${text}`);
+};
 
 const exitAsk = (e, win) => {
   e.preventDefault(); //阻止默认行为
   dialog
-      .showMessageBox({
-        type : 'info',
-        title : 'Information',
-        cancelId : 2,
-        defaultId : 0,
-        message : '确定要关闭吗？',
-        buttons : [ '最小化', '直接退出' ],
-      })
-      .then(result => {
-        if (result.response == 0) {
-          e.preventDefault(); //阻止默认行为
-          win.minimize();     //调用 最小化实例方法
-        } else if (result.response == 1) {
-          win = null;
-          // app.quit();
-          app.exit(); // exit()直接关闭客户端，不会执行quit();
-        }
-      })
-      .catch(err => { log(err); });
+    .showMessageBox({
+      type: 'info',
+      title: 'Information',
+      cancelId: 2,
+      defaultId: 0,
+      message: '确定要关闭吗？',
+      buttons: ['最小化', '直接退出'],
+    })
+    .then(result => {
+      if (result.response == 0) {
+        e.preventDefault(); //阻止默认行为
+        win.minimize(); //调用 最小化实例方法
+      } else if (result.response == 1) {
+        win = null;
+        // app.quit();
+        app.exit(); // exit()直接关闭客户端，不会执行quit();
+      }
+    })
+    .catch(err => {
+      log(err);
+    });
 };
 
 const exitAskWithoutMac = (e, win) => {
   e.preventDefault(); //阻止默认行为
   dialog
-      .showMessageBox({
-        type : 'info',
-        title : 'Information',
-        cancelId : 2,
-        defaultId : 0,
-        message : '确定要关闭吗？',
-        buttons : [ '最小化到托盘', '直接退出' ],
-        checkboxLabel : '记住我的选择',
-      })
-      .then(result => {
-        if (result.checkboxChecked && result.response !== 2) {
-          win.webContents.send('rememberCloseAppOption', result.response === 0
-                                                             ? 'minimizeToTray'
-                                                             : 'exit');
-        }
+    .showMessageBox({
+      type: 'info',
+      title: 'Information',
+      cancelId: 2,
+      defaultId: 0,
+      message: '确定要关闭吗？',
+      buttons: ['最小化到托盘', '直接退出'],
+      checkboxLabel: '记住我的选择',
+    })
+    .then(result => {
+      if (result.checkboxChecked && result.response !== 2) {
+        win.webContents.send(
+          'rememberCloseAppOption',
+          result.response === 0 ? 'minimizeToTray' : 'exit'
+        );
+      }
 
-        if (result.response === 0) {
-          e.preventDefault(); //阻止默认行为
-          win.hide();         //调用 最小化实例方法
-        } else if (result.response === 1) {
-          win = null;
-          // app.quit();
-          app.exit(); // exit()直接关闭客户端，不会执行quit();
-        }
-      })
-      .catch(err => { log(err); });
+      if (result.response === 0) {
+        e.preventDefault(); //阻止默认行为
+        win.hide(); //调用 最小化实例方法
+      } else if (result.response === 1) {
+        win = null;
+        // app.quit();
+        app.exit(); // exit()直接关闭客户端，不会执行quit();
+      }
+    })
+    .catch(err => {
+      log(err);
+    });
 };
 
 const client = require('discord-rich-presence')('818936529484906596');
@@ -92,11 +98,11 @@ function toBuffer(data) {
 async function getBiliVideoFile(url) {
   const axios = await import('axios').then(m => m.default);
   const response = await axios.get(url, {
-    headers : {
-      Referer : 'https://www.bilibili.com/',
-      'User-Agent' : 'okhttp/3.4.1',
+    headers: {
+      Referer: 'https://www.bilibili.com/',
+      'User-Agent': 'okhttp/3.4.1',
     },
-    responseType : 'arraybuffer',
+    responseType: 'arraybuffer',
   });
 
   const buffer = toBuffer(response.data);
@@ -115,40 +121,44 @@ async function getBiliVideoFile(url) {
 function parseSourceStringToList(executor, sourceString) {
   const availableSource = executor.list();
 
-  return sourceString.split(',').map(s => s.trim().toLowerCase()).filter(s => {
-    const isAvailable = availableSource.includes(s);
+  return sourceString
+    .split(',')
+    .map(s => s.trim().toLowerCase())
+    .filter(s => {
+      const isAvailable = availableSource.includes(s);
 
-    if (!isAvailable) {
-      log(`This source is not one of the supported source: ${s}`);
-    }
+      if (!isAvailable) {
+        log(`This source is not one of the supported source: ${s}`);
+      }
 
-    return isAvailable;
-  });
+      return isAvailable;
+    });
 }
 
 export function initIpcMain(win, store, trayEventEmitter) {
   // WIP: Do not enable logging as it has some issues in non-blocking I/O
   // environment. UNM.enableLogging(UNM.LoggingType.ConsoleEnv);
   const unmExecutor = new UNM.Executor();
-  const context = {enableFlac : true};
+  const context = { enableFlac: true };
 
   ipcMain.handle('unblock-music', async (_, track, source) => {
     const song = {
-      id : track.id && track.id.toString(),
-      name : track.name,
-      duration : track.dt,
-      album : track.al && {
-        id : track.al.id && track.al.id.toString(),
-        name : track.al.name,
+      id: track.id && track.id.toString(),
+      name: track.name,
+      duration: track.dt,
+      album: track.al && {
+        id: track.al.id && track.al.id.toString(),
+        name: track.al.name,
       },
-      artists : track.ar ? track.ar.map(({id, name}) =>
-                                            ({id : id && id.toString(), name}))
-                         : [],
+      artists: track.ar
+        ? track.ar.map(({ id, name }) => ({ id: id && id.toString(), name }))
+        : [],
     };
 
-    const sourceList = typeof source === 'string'
-                           ? parseSourceStringToList(unmExecutor, source)
-                           : [ 'kuwo', 'migu', 'ytdl', 'bilibili' ];
+    const sourceList =
+      typeof source === 'string'
+        ? parseSourceStringToList(unmExecutor, source)
+        : ['kuwo', 'migu', 'ytdl', 'bilibili'];
     log(`[UNM] using source: ${sourceList.join(', ')}`);
 
     try {
@@ -190,7 +200,9 @@ export function initIpcMain(win, store, trayEventEmitter) {
     }
   });
 
-  ipcMain.on('minimize', () => { win.minimize(); });
+  ipcMain.on('minimize', () => {
+    win.minimize();
+  });
 
   ipcMain.on('maximizeOrUnmaximize', () => {
     const isMaximized = win.isMaximized();
@@ -210,36 +222,40 @@ export function initIpcMain(win, store, trayEventEmitter) {
 
   ipcMain.on('playDiscordPresence', (event, track) => {
     client.updatePresence({
-      details : track.name + ' - ' + track.ar.map(ar => ar.name).join(','),
-      state : track.al.name,
-      endTimestamp : Date.now() + track.dt,
-      largeImageKey : 'logo',
-      largeImageText : 'Listening ' + track.name,
-      smallImageKey : 'play',
-      smallImageText : 'Playing',
-      instance : true,
+      details: track.name + ' - ' + track.ar.map(ar => ar.name).join(','),
+      state: track.al.name,
+      endTimestamp: Date.now() + track.dt,
+      largeImageKey: 'logo',
+      largeImageText: 'Listening ' + track.name,
+      smallImageKey: 'play',
+      smallImageText: 'Playing',
+      instance: true,
     });
   });
 
   ipcMain.on('pauseDiscordPresence', (event, track) => {
     client.updatePresence({
-      details : track.name + ' - ' + track.ar.map(ar => ar.name).join(','),
-      state : track.al.name,
-      largeImageKey : 'logo',
-      largeImageText : 'YesPlayMusic',
-      smallImageKey : 'pause',
-      smallImageText : 'Pause',
-      instance : true,
+      details: track.name + ' - ' + track.ar.map(ar => ar.name).join(','),
+      state: track.al.name,
+      largeImageKey: 'logo',
+      largeImageText: 'YesPlayMusic',
+      smallImageKey: 'pause',
+      smallImageText: 'Pause',
+      instance: true,
     });
   });
 
   ipcMain.on('setProxy', (event, config) => {
     const proxyRules = `${config.protocol}://${config.server}:${config.port}`;
     store.set('proxy', proxyRules);
-    win.webContents.session.setProxy({
-      proxyRules,
-    },
-                                     () => { log('finished setProxy'); });
+    win.webContents.session.setProxy(
+      {
+        proxyRules,
+      },
+      () => {
+        log('finished setProxy');
+      }
+    );
   });
 
   ipcMain.on('removeProxy', (event, arg) => {
@@ -257,7 +273,7 @@ export function initIpcMain(win, store, trayEventEmitter) {
     }
   });
 
-  ipcMain.on('updateShortcut', (e, {id, type, shortcut}) => {
+  ipcMain.on('updateShortcut', (e, { id, type, shortcut }) => {
     log('updateShortcut');
     let shortcuts = store.get('settings.shortcuts');
     let newShortcut = shortcuts.find(s => s.id === id);
@@ -279,14 +295,14 @@ export function initIpcMain(win, store, trayEventEmitter) {
   });
 
   if (isCreateTray) {
-    ipcMain.on(
-        'updateTrayTooltip',
-        (_, title) => { trayEventEmitter.emit('updateTooltip', title); });
+    ipcMain.on('updateTrayTooltip', (_, title) => {
+      trayEventEmitter.emit('updateTooltip', title);
+    });
     ipcMain.on('updateTrayPlayState', (_, isPlaying) => {
       trayEventEmitter.emit('updatePlayState', isPlaying);
     });
-    ipcMain.on(
-        'updateTrayLikeState',
-        (_, isLiked) => { trayEventEmitter.emit('updateLikeState', isLiked); });
+    ipcMain.on('updateTrayLikeState', (_, isLiked) => {
+      trayEventEmitter.emit('updateLikeState', isLiked);
+    });
   }
 }
