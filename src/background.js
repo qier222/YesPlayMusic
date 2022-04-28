53d51cbbb07d3306c166825ca3cc398eb5e577e4
Restyled.io <commits@restyled.io>
2022-04-28 13:02:50 +0000

    Restyled by prettier

--- src/background.js
+++ src/background.js
@@ -17,24 +17,25 @@ import {
   isCreateTray,
   isCreateMpris,
 } from '@/utils/platform';
-import {createProtocol} from 'vue-cli-plugin-electron-builder/lib';
-import {startNeteaseMusicApi} from './electron/services';
-import {initIpcMain} from './electron/ipcMain.js';
-import {createMenu} from './electron/menu';
-import {createTray} from '@/electron/tray';
-import {createTouchBar} from './electron/touchBar';
-import {createDockMenu} from './electron/dockMenu';
-import {registerGlobalShortcut} from './electron/globalShortcut';
-import {autoUpdater} from 'electron-updater';
-import installExtension, {VUEJS_DEVTOOLS} from 'electron-devtools-installer';
-import {EventEmitter} from 'events';
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
+import { startNeteaseMusicApi } from './electron/services';
+import { initIpcMain } from './electron/ipcMain.js';
+import { createMenu } from './electron/menu';
+import { createTray } from '@/electron/tray';
+import { createTouchBar } from './electron/touchBar';
+import { createDockMenu } from './electron/dockMenu';
+import { registerGlobalShortcut } from './electron/globalShortcut';
+import { autoUpdater } from 'electron-updater';
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
+import { EventEmitter } from 'events';
 import express from 'express';
 import expressProxy from 'express-http-proxy';
 import Store from 'electron-store';
-import {createMpris} from '@/electron/mpris';
+import { createMpris } from '@/electron/mpris';
 const clc = require('cli-color');
-const log =
-    text => { console.log(`${clc.blueBright('[background.js]')} ${text}`); };
+const log = text => {
+  console.log(`${clc.blueBright('[background.js]')} ${text}`);
+};
 
 const closeOnLinux = (e, win, store) => {
   let closeOpt = store.get('settings.closeAppOption');
@@ -44,30 +45,33 @@ const closeOnLinux = (e, win, store) => {
 
   if (closeOpt === 'ask') {
     dialog
-        .showMessageBox({
-          type : 'info',
-          title : 'Information',
-          cancelId : 2,
-          defaultId : 0,
-          message : '确定要关闭吗？',
-          buttons : [ '最小化到托盘', '直接退出' ],
-          checkboxLabel : '记住我的选择',
-        })
-        .then(result => {
-          if (result.checkboxChecked && result.response !== 2) {
-            win.webContents.send('rememberCloseAppOption',
-                                 result.response === 0 ? 'minimizeToTray'
-                                                       : 'exit');
-          }
+      .showMessageBox({
+        type: 'info',
+        title: 'Information',
+        cancelId: 2,
+        defaultId: 0,
+        message: '确定要关闭吗？',
+        buttons: ['最小化到托盘', '直接退出'],
+        checkboxLabel: '记住我的选择',
+      })
+      .then(result => {
+        if (result.checkboxChecked && result.response !== 2) {
+          win.webContents.send(
+            'rememberCloseAppOption',
+            result.response === 0 ? 'minimizeToTray' : 'exit'
+          );
+        }
 
-          if (result.response === 0) {
-            win.hide(); //调用 最小化实例方法
-          } else if (result.response === 1) {
-            win = null;
-            app.exit(); // exit()直接关闭客户端，不会执行quit();
-          }
-        })
-        .catch(err => { log(err); });
+        if (result.response === 0) {
+          win.hide(); //调用 最小化实例方法
+        } else if (result.response === 1) {
+          win = null;
+          app.exit(); // exit()直接关闭客户端，不会执行quit();
+        }
+      })
+      .catch(err => {
+        log(err);
+      });
   } else if (closeOpt === 'exit') {
     win = null;
     app.quit();
@@ -81,9 +85,9 @@ class Background {
     this.window = null;
     this.ypmTrayImpl = null;
     this.store = new Store({
-      windowWidth : {
-        width : {type : 'number', default : 1440},
-        height : {type : 'number', default : 840},
+      windowWidth: {
+        width: { type: 'number', default: 1440 },
+        height: { type: 'number', default: 840 },
       },
     });
     this.neteaseMusicAPI = null;
@@ -97,8 +101,7 @@ class Background {
     log('initializing');
 
     // Make sure the app is singleton.
-    if (!app.requestSingleInstanceLock())
-      return app.quit();
+    if (!app.requestSingleInstanceLock()) return app.quit();
 
     // start netease music api
     this.neteaseMusicAPI = startNeteaseMusicApi();
@@ -108,7 +111,7 @@ class Background {
 
     // Scheme must be registered before the app is ready
     protocol.registerSchemesAsPrivileged([
-      {scheme : 'app', privileges : {secure : true, standard : true}},
+      { scheme: 'app', privileges: { secure: true, standard: true } },
     ]);
 
     // handle app events
@@ -117,7 +120,9 @@ class Background {
     // disable chromium mpris
     if (isCreateMpris) {
       app.commandLine.appendSwitch(
-          'disable-features', 'HardwareMediaKeyHandling,MediaSessionService');
+        'disable-features',
+        'HardwareMediaKeyHandling,MediaSessionService'
+      );
     }
   }
 
@@ -137,7 +142,9 @@ class Background {
         }
       });
     } else {
-      process.on('SIGTERM', () => { app.quit(); });
+      process.on('SIGTERM', () => {
+        app.quit();
+      });
     }
   }
 
@@ -148,14 +155,16 @@ class Background {
     expressApp.use('/', express.static(__dirname + '/'));
     expressApp.use('/api', expressProxy('http://127.0.0.1:10754'));
     expressApp.use('/player', (req, res) => {
-      this.window.webContents.executeJavaScript('window.yesplaymusic.player')
-          .then(result => {
-            res.send({
-              currentTrack : result._isPersonalFM ? result._personalFMTrack
-                                                  : result._currentTrack,
-              progress : result._progress,
-            });
+      this.window.webContents
+        .executeJavaScript('window.yesplaymusic.player')
+        .then(result => {
+          res.send({
+            currentTrack: result._isPersonalFM
+              ? result._personalFMTrack
+              : result._currentTrack,
+            progress: result._progress,
           });
+        });
     });
     this.expressApp = expressApp.listen(27232, '127.0.0.1');
   }
@@ -167,27 +176,29 @@ class Background {
     const showLibraryDefault = this.store.get('settings.showLibraryDefault');
 
     const options = {
-      width : this.store.get('window.width') || 1440,
-      height : this.store.get('window.height') || 840,
-      minWidth : 1080,
-      minHeight : 720,
-      titleBarStyle : 'hiddenInset',
-      frame :
-          !(isWindows ||
-            (isLinux && this.store.get('settings.linuxEnableCustomTitlebar'))),
-      title : 'YesPlayMusic',
-      show : false,
-      webPreferences : {
-        webSecurity : false,
-        nodeIntegration : true,
-        enableRemoteModule : true,
-        contextIsolation : false,
+      width: this.store.get('window.width') || 1440,
+      height: this.store.get('window.height') || 840,
+      minWidth: 1080,
+      minHeight: 720,
+      titleBarStyle: 'hiddenInset',
+      frame: !(
+        isWindows ||
+        (isLinux && this.store.get('settings.linuxEnableCustomTitlebar'))
+      ),
+      title: 'YesPlayMusic',
+      show: false,
+      webPreferences: {
+        webSecurity: false,
+        nodeIntegration: true,
+        enableRemoteModule: true,
+        contextIsolation: false,
       },
-      backgroundColor : ((appearance === undefined || appearance === 'auto') &&
-                         nativeTheme.shouldUseDarkColors) ||
-                                appearance === 'dark'
-                            ? '#222'
-                            : '#fff',
+      backgroundColor:
+        ((appearance === undefined || appearance === 'auto') &&
+          nativeTheme.shouldUseDarkColors) ||
+        appearance === 'dark'
+          ? '#222'
+          : '#fff',
     };
 
     if (this.store.get('window.x') && this.store.get('window.y')) {
@@ -197,17 +208,25 @@ class Background {
       let displays = screen.getAllDisplays();
       let isResetWindiw = false;
       if (displays.length === 1) {
-        let {bounds} = displays[0];
-        if (x < bounds.x || x > bounds.x + bounds.width - 50 || y < bounds.y ||
-            y > bounds.y + bounds.height - 50) {
+        let { bounds } = displays[0];
+        if (
+          x < bounds.x ||
+          x > bounds.x + bounds.width - 50 ||
+          y < bounds.y ||
+          y > bounds.y + bounds.height - 50
+        ) {
           isResetWindiw = true;
         }
       } else {
         isResetWindiw = true;
         for (let i = 0; i < displays.length; i++) {
-          let {bounds} = displays[i];
-          if (x > bounds.x && x < bounds.x + bounds.width && y > bounds.y &&
-              y < bounds.y - bounds.height) {
+          let { bounds } = displays[i];
+          if (
+            x > bounds.x &&
+            x < bounds.x + bounds.width &&
+            y > bounds.y &&
+            y < bounds.y - bounds.height
+          ) {
             // 检测到APP窗口当前处于一个可用的屏幕里，break
             isResetWindiw = false;
             break;
@@ -229,44 +248,48 @@ class Background {
     if (process.env.WEBPACK_DEV_SERVER_URL) {
       // Load the url of the dev server if in development mode
       this.window.loadURL(
-          showLibraryDefault ? `${process.env.WEBPACK_DEV_SERVER_URL}/#/library`
-                             : process.env.WEBPACK_DEV_SERVER_URL);
-      if (!process.env.IS_TEST)
-        this.window.webContents.openDevTools();
+        showLibraryDefault
+          ? `${process.env.WEBPACK_DEV_SERVER_URL}/#/library`
+          : process.env.WEBPACK_DEV_SERVER_URL
+      );
+      if (!process.env.IS_TEST) this.window.webContents.openDevTools();
     } else {
       createProtocol('app');
-      this.window.loadURL(showLibraryDefault
-                              ? 'http://localhost:27232/#/library'
-                              : 'http://localhost:27232');
+      this.window.loadURL(
+        showLibraryDefault
+          ? 'http://localhost:27232/#/library'
+          : 'http://localhost:27232'
+      );
     }
   }
 
   checkForUpdates() {
-    if (isDevelopment)
-      return;
+    if (isDevelopment) return;
     log('checkForUpdates');
     autoUpdater.checkForUpdatesAndNotify();
 
     const showNewVersionMessage = info => {
       dialog
-          .showMessageBox({
-            title : '发现新版本 v' + info.version,
-            message : '发现新版本 v' + info.version,
-            detail : '是否前往 GitHub 下载新版本安装包？',
-            buttons : [ '下载', '取消' ],
-            type : 'question',
-            noLink : true,
-          })
-          .then(result => {
-            if (result.response === 0) {
-              shell.openExternal(
-                  'https://github.com/qier222/YesPlayMusic/releases');
-            }
-          });
+        .showMessageBox({
+          title: '发现新版本 v' + info.version,
+          message: '发现新版本 v' + info.version,
+          detail: '是否前往 GitHub 下载新版本安装包？',
+          buttons: ['下载', '取消'],
+          type: 'question',
+          noLink: true,
+        })
+        .then(result => {
+          if (result.response === 0) {
+            shell.openExternal(
+              'https://github.com/qier222/YesPlayMusic/releases'
+            );
+          }
+        });
     };
 
-    autoUpdater.on('update-available',
-                   info => { showNewVersionMessage(info); });
+    autoUpdater.on('update-available', info => {
+      showNewVersionMessage(info);
+    });
   }
 
   handleWindowEvents() {
@@ -301,37 +324,38 @@ class Background {
       }
     });
 
-    this.window.on(
-        'resized',
-        () => { this.store.set('window', this.window.getBounds()); });
+    this.window.on('resized', () => {
+      this.store.set('window', this.window.getBounds());
+    });
 
-    this.window.on(
-        'moved', () => { this.store.set('window', this.window.getBounds()); });
+    this.window.on('moved', () => {
+      this.store.set('window', this.window.getBounds());
+    });
 
-    this.window.on(
-        'maximize',
-        () => { this.window.webContents.send('isMaximized', true); });
+    this.window.on('maximize', () => {
+      this.window.webContents.send('isMaximized', true);
+    });
 
-    this.window.on(
-        'unmaximize',
-        () => { this.window.webContents.send('isMaximized', false); });
+    this.window.on('unmaximize', () => {
+      this.window.webContents.send('isMaximized', false);
+    });
 
-    this.window.webContents.on('new-window', function(e, url) {
+    this.window.webContents.on('new-window', function (e, url) {
       e.preventDefault();
       log('open url');
-      const excludeHosts = [ 'www.last.fm' ];
+      const excludeHosts = ['www.last.fm'];
       const exclude = excludeHosts.find(host => url.includes(host));
       if (exclude) {
         const newWindow = new BrowserWindow({
-          width : 800,
-          height : 600,
-          titleBarStyle : 'default',
-          title : 'YesPlayMusic',
-          webPreferences : {
-            webSecurity : false,
-            nodeIntegration : true,
-            enableRemoteModule : true,
-            contextIsolation : false,
+          width: 800,
+          height: 600,
+          titleBarStyle: 'default',
+          title: 'YesPlayMusic',
+          webPreferences: {
+            webSecurity: false,
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false,
           },
         });
         newWindow.loadURL(url);
@@ -355,7 +379,9 @@ class Background {
 
       // create window
       this.createWindow();
-      this.window.once('ready-to-show', () => { this.window.show(); });
+      this.window.once('ready-to-show', () => {
+        this.window.show();
+      });
       this.handleWindowEvents();
 
       // create tray
@@ -370,8 +396,9 @@ class Background {
       // set proxy
       const proxyRules = this.store.get('proxy');
       if (proxyRules) {
-        this.window.webContents.session.setProxy(
-            {proxyRules}, result => { log('finished setProxy', result); });
+        this.window.webContents.session.setProxy({ proxyRules }, result => {
+          log('finished setProxy', result);
+        });
       }
 
       // check for updates
@@ -382,13 +409,11 @@ class Background {
 
       // create dock menu for macOS
       const createdDockMenu = createDockMenu(this.window);
-      if (createDockMenu && app.dock)
-        app.dock.setMenu(createdDockMenu);
+      if (createDockMenu && app.dock) app.dock.setMenu(createdDockMenu);
 
       // create touch bar
       const createdTouchBar = createTouchBar(this.window);
-      if (createdTouchBar)
-        this.window.setTouchBar(createdTouchBar);
+      if (createdTouchBar) this.window.setTouchBar(createdTouchBar);
 
       // register global shortcuts
       if (this.store.get('settings.enableGlobalShortcut') !== false) {
@@ -418,9 +443,13 @@ class Background {
       }
     });
 
-    app.on('before-quit', () => { this.willQuitApp = true; });
+    app.on('before-quit', () => {
+      this.willQuitApp = true;
+    });
 
-    app.on('quit', () => { this.expressApp.close(); });
+    app.on('quit', () => {
+      this.expressApp.close();
+    });
 
     app.on('will-quit', () => {
       // unregister all global shortcuts
