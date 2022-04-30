8d7b467876a78e715648e6d112cb4fd578fe8e96
memorydream <34763046+memorydream@users.noreply.github.com>
2022-04-30 15:07:41 +0800

    fix: CoverColor在无法获取歌曲封面资源时使用备用图标的颜色

--- src/renderer/hooks/useCoverColor.ts
+++ src/renderer/hooks/useCoverColor.ts
@@ -1,14 +1,21 @@
 import { colord } from 'colord'
 import { getCoverColor } from '../utils/common'
 
-export default function useCoverColor(url: string) {
+export default function useCoverColor(url: string, fallbackColor?: string) {
   const [color, setColor] = useState({ from: '#fff', to: '#fff' })
+
+  const updateColor = (color?: string) => {
+    if (!color) return
+    const to = colord(color).darken(0.15).rotate(-5).toHex()
+    setColor({ from: color, to })
+  }
 
   useEffect(() => {
     getCoverColor(url || '').then(color => {
-      if (!color) return
-      const to = colord(color).darken(0.15).rotate(-5).toHex()
-      setColor({ from: color, to })
+      updateColor(color)
+    })
+    .catch(() => {
+      updateColor(fallbackColor)
     })
   }, [url])
 
