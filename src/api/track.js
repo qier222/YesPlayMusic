a9d31b54711d622f32857394a73cc073d643edf3
Restyled.io <commits@restyled.io>
2022-05-01 06:13:05 +0000

    Restyled by clang-format

--- src/api/track.js
+++ src/api/track.js
@@ -1,16 +1,17 @@
 import store from '@/store';
-import request from '@/utils/request';
-import { mapTrackPlayableStatus } from '@/utils/common';
+import {mapTrackPlayableStatus} from '@/utils/common';
 import {
-  cacheTrackDetail,
-  getTrackDetailFromCache,
   cacheLyric,
+  cacheTrackDetail,
   getLyricFromCache,
+  getTrackDetailFromCache,
 } from '@/utils/db';
+import request from '@/utils/request';
 
 /**
  * 获取音乐 url
- * 说明 : 使用歌单详情接口后 , 能得到的音乐的 id, 但不能得到的音乐 url, 调用此接口, 传入的音乐 id( 可多个 , 用逗号隔开 ), 可以获取对应的音乐的 url,
+ * 说明 : 使用歌单详情接口后 , 能得到的音乐的 id, 但不能得到的音乐 url,
+ * 调用此接口, 传入的音乐 id( 可多个 , 用逗号隔开 ), 可以获取对应的音乐的 url,
  * !!!未登录状态返回试听片段(返回字段包含被截取的正常歌曲的开始时间和结束时间)
  * @param {string} id - 音乐的 id，例如 id=405998841,33894312
  */
@@ -22,40 +23,42 @@ export function getMP3(id) {
   };
 
   return request({
-    url: '/song/url',
-    method: 'get',
-    params: {
+    url : '/song/url',
+    method : 'get',
+    params : {
       id,
-      br: getBr(),
+      br : getBr(),
     },
   });
 }
 
 /**
  * 获取歌曲详情
- * 说明 : 调用此接口 , 传入音乐 id(支持多个 id, 用 , 隔开), 可获得歌曲详情(注意:歌曲封面现在需要通过专辑内容接口获取)
+ * 说明 : 调用此接口 , 传入音乐 id(支持多个 id, 用 , 隔开),
+ * 可获得歌曲详情(注意:歌曲封面现在需要通过专辑内容接口获取)
  * @param {string} ids - 音乐 id, 例如 ids=405998841,33894312
  */
 export function getTrackDetail(ids) {
   const fetchLatest = () => {
     return request({
-      url: '/song/detail',
-      method: 'get',
-      params: {
-        ids,
-      },
-    }).then(data => {
-      data.songs.map(song => {
-        const privileges = data.privileges.find(t => t.id === song.id);
-        cacheTrackDetail(song, privileges);
-      });
-      data.songs = mapTrackPlayableStatus(data.songs, data.privileges);
-      return data;
-    });
+             url : '/song/detail',
+             method : 'get',
+             params : {
+               ids,
+             },
+           })
+        .then(data => {
+          data.songs.map(song => {
+            const privileges = data.privileges.find(t => t.id === song.id);
+            cacheTrackDetail(song, privileges);
+          });
+          data.songs = mapTrackPlayableStatus(data.songs, data.privileges);
+          return data;
+        });
   };
   fetchLatest();
 
-  let idsInArray = [String(ids)];
+  let idsInArray = [ String(ids) ];
   if (typeof ids === 'string') {
     idsInArray = ids.split(',');
   }
@@ -76,34 +79,35 @@ export function getTrackDetail(ids) {
 export function getLyric(id) {
   const fetchLatest = () => {
     return request({
-      url: '/lyric',
-      method: 'get',
-      params: {
-        id,
-      },
-    }).then(result => {
-      cacheLyric(id, result);
-      return result;
-    });
+             url : '/lyric',
+             method : 'get',
+             params : {
+               id,
+             },
+           })
+        .then(result => {
+          cacheLyric(id, result);
+          return result;
+        });
   };
 
   fetchLatest();
 
-  return getLyricFromCache(id).then(result => {
-    return result ?? fetchLatest();
-  });
+  return getLyricFromCache(id).then(
+      result => { return result ?? fetchLatest(); });
 }
 
 /**
  * 新歌速递
  * 说明 : 调用此接口 , 可获取新歌速递
- * @param {number} type - 地区类型 id, 对应以下: 全部:0 华语:7 欧美:96 日本:8 韩国:16
+ * @param {number} type - 地区类型 id, 对应以下: 全部:0 华语:7 欧美:96 日本:8
+ *     韩国:16
  */
 export function topSong(type) {
   return request({
-    url: '/top/song',
-    method: 'get',
-    params: {
+    url : '/top/song',
+    method : 'get',
+    params : {
       type,
     },
   });
@@ -121,8 +125,8 @@ export function topSong(type) {
 export function likeATrack(params) {
   params.timestamp = new Date().getTime();
   return request({
-    url: '/like',
-    method: 'get',
+    url : '/like',
+    method : 'get',
     params,
   });
 }
@@ -141,8 +145,8 @@ export function likeATrack(params) {
 export function scrobble(params) {
   params.timestamp = new Date().getTime();
   return request({
-    url: '/scrobble',
-    method: 'get',
+    url : '/scrobble',
+    method : 'get',
     params,
   });
 }
