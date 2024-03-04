b0ed85689bf107eda17981d6d86a448db4903efc
代强 <daiqianghaha@foxmail.com>
2024-03-04 19:24:10 +0800

    chore: remove useless space

--- src/electron/services.js
+++ src/electron/services.js
@@ -3,7 +3,7 @@ import checkAuthToken from "../utils/checkAuthToken"
 import server from 'NeteaseCloudMusicApi/server';
 
 export async function startNeteaseMusicApi() {
-      // Let user know that the service is starting
+  // Let user know that the service is starting
   console.log(`${clc.redBright('[NetEase API]')} initiating NCM API`);
 
   // Load the NCM API.
