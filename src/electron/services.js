a18e093d4adf29a207c767da256d1ce45adc2baf
代强 <daiqianghaha@foxmail.com>
2024-03-04 19:22:47 +0800

    fix: fix build && adapte for bugs in NeteasyCloudMusicAPI

--- src/electron/services.js
+++ src/electron/services.js
@@ -1,8 +1,9 @@
 import clc from 'cli-color';
+import checkAuthToken from "../utils/checkAuthToken"
 import server from 'NeteaseCloudMusicApi/server';
 
 export async function startNeteaseMusicApi() {
-  // Let user know that the service is starting
+      // Let user know that the service is starting
   console.log(`${clc.redBright('[NetEase API]')} initiating NCM API`);
 
   // Load the NCM API.
