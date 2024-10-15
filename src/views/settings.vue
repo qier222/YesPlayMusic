d219e485418edef822d7785fe100dbcd72fb3ee8
StarsEnd <starsend33a2d17@outlook.com>
2024-10-16 00:14:38 +0800

    fix: real-ip config disabled css style

--- src/views/settings.vue
+++ src/views/settings.vue
@@ -1615,11 +1615,13 @@ input[type='number'] {
   -moz-appearance: textfield;
 }
 
-#proxy-form {
+#proxy-form,
+#real-ip {
   display: flex;
   align-items: center;
 }
-#proxy-form.disabled {
+#proxy-form.disabled,
+#real-ip.disabled {
   opacity: 0.47;
   button:hover {
     transform: unset;
