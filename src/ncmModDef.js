b389a95f0aedabbc502187d79fcd8bf1dca22014
Luorix <157946924+LuorixDev@users.noreply.github.com>
2025-11-02 16:15:54 +0800

    Update ncmModDef.js

--- src/ncmModDef.js
+++ src/ncmModDef.js
@@ -369,6 +369,11 @@ module.exports = [
     route: '/lyric',
     module: require('@neteaseapireborn/api/module/lyric'),
   },
+  {  
+  identifier: 'lyric_new',  
+  route: '/lyric/new',  
+  module: require('NeteaseCloudMusicApi/module/lyric_new'),  
+  },
   {
     identifier: 'logout',
     route: '/logout',
